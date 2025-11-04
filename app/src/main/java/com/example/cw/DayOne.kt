a05a7b0b3d3f7ed2f2f335f15ea2a14590eb249e
NirdeshKajiShakya <nirkajishakya23@gmail.com>
2025-11-04 14:25:38 +0545

    First commit

---
+++ app/src/main/java/com/example/cw/DayOne.kt
@@ -0,0 +1,38 @@
+package com.example.cw
+
+fun main() {
+    //cw1()
+    cw2()
+}
+fun cw1(){
+    //mutable
+    var something : String = "someone"
+    something = "ram"
+    //immutable
+    val age : Int = 15
+
+    println("mero nam ${something.uppercase()} ho ani mero age chai $age ho")
+}
+fun cw2(){
+    val age = arrayOf(10,20,30)
+    age[2] = 50
+    println(age[2])
+
+    val age1 = ArrayList<Int>()
+    val age2 = arrayListOf<Int>(10,20,30)
+    age1.add(5)
+    age2.add(40)
+
+    println(age1)
+    println(age2)
+
+    for(i in 0 .. 10 step 1){
+        print("  $i")
+    }
+
+    println("")
+    
+    for(i in 10 downTo 0 step 2){
+        print("  $i")
+    }
+}
